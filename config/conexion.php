58fe7899d1fbf18b7cce9aeb60ba6ca9d219dea5
gaspcr <riugarte@uc.cl>
2022-11-06 15:31:42 -0300

    dsad

--- config/conexion.php
+++ config/conexion.php
@@ -3,7 +3,7 @@
     #Pide las variables para conectarse a la base de datos.
     require('data.php'); 
     # Se crea la instancia de PDO
-    $db = new PDO("pgsql:dbname=$databaseName;user=$user;password=$password");
+    $db = new PDO("pgsql:dbname=grupo93e2;user=grupo93;password=astro123");
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e";
   }
